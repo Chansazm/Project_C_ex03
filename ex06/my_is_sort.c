2838be74271cca8045decc07f2ec6011872f9d9b
Chansa CHISHIMBA <chishimb_c@qwasar.io>
2020-05-08 15:29:41 +0000

    Tried to use recursion

---
+++ ex06/my_is_sort.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+/*
+typedef struct integer_array
+{
+    int size;
+    int* array; 
+}integer_array;
+*/
+bool my_is_sort(integer_array * param_1) 
+{
+    
+    while (param_1->size <= 1)
+        return 1;
+        if (param_1->size -1 >= param_1->size - 2)
+        return true;
+    
+    return false; 
+}
+
+
+//[4,7,0,3]---> False
+
+
